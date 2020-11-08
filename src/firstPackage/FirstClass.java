1bd945a007fc8d35f4dbbc477cbba706b5eb00c2
FurkanKara98 <73558405+FurkanKara98@users.noreply.github.com>
2020-11-08 15:36:21 -0500

    Update FirstClass.java
    here is the last update about conflict on remote master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -24,7 +24,7 @@ public class FirstClass {
 		
                 System.out.println("Here are some changes from remote master branch");
 		
-		System.out.println("Here is my second conflict from remote feature branch")
+		System.out.println("Here is my second conflict from remote master branch")
 	        
 		
 
