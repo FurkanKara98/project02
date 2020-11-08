cb7f114c2f71ee60eb9a5f84365e46f807ee2f6a
Furkan <Furk98K@gmail.com>
2020-11-08 11:16:17 -0500

    third commit from local master

--- src/firstPackage/FirstClass.java
+++ src/firstPackage/FirstClass.java
@@ -4,6 +4,8 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("first commit from local master branch");
+		System.out.println("second commit from local master branch");
+		System.out.println("third commit from local master");
 
 	}
 
